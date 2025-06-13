7ea5f7b0dade2c20a0205bc0024a7fb72cc630e3
clownpred <calkesha@mail.ru>
2025-06-14 01:35:52 +0500

    Update amongus_scan.py

--- src/amongus_scan.py
+++ src/amongus_scan.py
@@ -2,10 +2,10 @@ import random
 
 def scan_network():
     sus_hosts = ["10.0.0.14", "192.168.1.6", "172.16.42.69"]
-    print("🧑‍🚀 Starting SussyScan™...")
+    print(" Starting SussyScan™...")
     for host in sus_hosts:
-        print(f"🔍 Found sus activity on {host} (Role: {random.choice(['Impostor', 'Crewmate'])})")
-    print("✅ Scan complete. Report sent to HQ on Skeld.")
+        print(f" Found sus activity on {host} (Role: {random.choice(['Impostor', 'Crewmate'])})")
+    print(" Scan complete. Report sent to HQ on Skeld.")
 
 if __name__ == "__main__":
     scan_network()
